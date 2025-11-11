d9ba64095e8e3e00f979d16d95997336e5cd9b45
nestoque <ninjaze.ne@gmail.com>
2025-11-10 20:11:27 -0700

    p5 started serverFacade

---
+++ client/src/main/java/serverfacade/ServerFacade.java
@@ -0,0 +1,111 @@
+package serverfacade;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import requests.*;
+import responses.*;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws ResponseException {
+        var request = buildRequest("POST", "/pet", pet);
+        var response = sendRequest(request);
+        return handleResponse(response, Pet.class);
+    }
+
+    public LoginResult login(LoginRequest request) throws ResponseException {
+        var path = String.format("/pet/%s", id);
+        var request = buildRequest("DELETE", path, null);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public void logout(String authToken) throws ResponseException {
+        var request = buildRequest("GET", "/pet", null);
+        var response = sendRequest(request);
+        return handleResponse(response, PetList.class);
+    }
+
+    public CreateGameResult createGame(String authToken, CreateGameRequest req) throws ResponseException {
+        var path = String.format("/pet/%s", id);
+        var request = buildRequest("DELETE", path, null);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public void joinGame(String authToken, JoinGameRequest req) throws ResponseException {
+        var request = buildRequest("DELETE", "/pet", req);
+        sendRequest(request);
+    }
+
+
+    public ListGamesResult listGames(String authToken) throws ResponseException {
+        var request = buildRequest("GET", "/game", authToken);
+        var response = sendRequest(request);
+        return handleResponse(response, ListGamesResult.class);
+    }
+
+    public void clear() throws ResponseException {
+        var request = buildRequest("DELETE", "/db", null);
+        var response = sendRequest(request);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private HttpRequest.BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return HttpRequest.BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+
+            throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
