b94063479cb539050b360f3a451bcaf68636c579
nestoque <ninjaze.ne@gmail.com>
2025-11-14 17:29:30 -0700

    p5 before quality

--- client/src/main/java/ui/PostLoginClient.java
+++ client/src/main/java/ui/PostLoginClient.java
@@ -45,8 +45,10 @@ public class PostLoginClient {
     public ReplResult list(String... params) throws ResponseException {
         responses.ListGamesResult games = server.listGames(authToken);
         StringBuilder outString = new StringBuilder();
+        int cnt = 1;
         for (ListGameArrayResult game : games.games()) {
-            outString.append(String.format("ID: %d\tName: %s\tWhite: %s\tBlack: %s\n", game.gameID(), game.gameName(), printUser(game.whiteUsername()), printUser(game.blackUsername())));
+            outString.append(String.format("#: %d\tName: %s\tWhite: %s\tBlack: %s\n", cnt, game.gameName(), printUser(game.whiteUsername()), printUser(game.blackUsername())));
+            cnt++;
         }
 
         return new ReplResult(outString.toString(), ReplResult.State.POSTLOGIN);
@@ -61,16 +63,29 @@ public class PostLoginClient {
         throw new ResponseException(ResponseException.Code.ClientError, "Expected: <Game Name>");
     }
 
+    //game ID here SHOULD BE INDEPENDENT
     public ReplResult join(String... params) throws ResponseException {
         try {
-            if (params.length == 2 && Integer.parseInt(params[0]) < 1) {
-                server.joinGame(authToken, new JoinGameRequest(params[1].toUpperCase(), Integer.parseInt(params[0])));
+            int printedGameNum = Integer.parseInt(params[0]);
+            if (params.length == 2 && printedGameNum >= 1) {
+                ListGamesResult gamesList = server.listGames(authToken);
+                int cnt = 1;
+                int actualGameID = -1;
+                for (ListGameArrayResult game : gamesList.games()) {
+                    if (cnt == printedGameNum) {
+                        actualGameID = game.gameID();
+                        break;
+                    } else {
+                        cnt++;
+                    }
+                }
+                server.joinGame(authToken, new JoinGameRequest(params[1].toUpperCase(), actualGameID));
                 joinedColor = params[1].toUpperCase();
                 return new ReplResult(String.format("Join Game #%s as %s.\n", params[0], params[1]), ReplResult.State.GAME);
             }
             throw new ResponseException(ResponseException.Code.ClientError, "Expected: <Game #> <WHITE/BLACK>");
         } catch (Exception e) {
-            throw new ResponseException(ResponseException.Code.ClientError, e.getMessage());
+            throw new ResponseException(ResponseException.Code.ClientError, "Expected: <Game #> <WHITE/BLACK>" + e.getMessage());
         }
 
 
@@ -78,9 +93,21 @@ public class PostLoginClient {
 
     public ReplResult watch(String... params) throws ResponseException {
         try {
-            if (params.length == 1 && Integer.parseInt(params[0]) < 1) {
+            int printedGameNum = Integer.parseInt(params[0]);
+            if (params.length == 1 && printedGameNum >= 1) {
+                ListGamesResult gamesList = server.listGames(authToken);
+                int cnt = 1;
+                int actualGameID = -1;
+                for (ListGameArrayResult game : gamesList.games()) {
+                    if (cnt == printedGameNum) {
+                        actualGameID = game.gameID();
+                        break;
+                    } else {
+                        cnt++;
+                    }
+                }
                 joinedColor = null;
-                return new ReplResult(String.format("observing Game # %s.\n", params[0]), ReplResult.State.GAME);
+                return new ReplResult(String.format("observing Game # %s.\n", actualGameID), ReplResult.State.GAME);
             }
             throw new ResponseException(ResponseException.Code.ClientError, "Expected: <Game #>");
         } catch (Exception e) {
